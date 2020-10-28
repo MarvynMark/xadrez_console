e322f1d446a6c828cf0e78c1262ee9af5ff1d89c
Marvyn Mark <marvyn.mark@gmail.com>
2020-10-27 21:39:45 -0300

    Implementando a logica de xequemate

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -16,9 +16,6 @@ namespace xadrez_console {
                         Console.Clear();
                         Tela.imprimirPartida(partida);
 
-                  
-                       
-
                         Console.WriteLine();
                         Console.Write("Origem: ");
                         Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
@@ -41,8 +38,8 @@ namespace xadrez_console {
                         Console.ReadLine();
                     }
                 }
-                
-
+                Console.Clear();
+                Tela.imprimirPartida(partida);
             }
             catch (TabuleiroException e) {
                 Console.WriteLine(e.Message);
